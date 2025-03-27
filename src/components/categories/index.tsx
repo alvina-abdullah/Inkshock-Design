61d28e89b82d18efaf7a1af0e3072feba5d38326
Alvina Abdullah <alvinaabdullah16@gmail.com>
2025-03-27 15:27:07 +0500

    add images

--- src/components/categories/index.tsx
+++ src/components/categories/index.tsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 
 const categories = [
-  { name: "T-Shirts", image: "/images/tshirt.png", bg: "bg-green-900" },
-  { name: "Shirts", image: "/images/shirt.png", bg: "bg-gray-600" },
-  { name: "Hoodies", image: "/images/hoodies.png", bg: "bg-gray-300" },
-  { name: "Caps", image: "/images/caps.png", bg: "bg-white" },
+  { name: "T-Shirts", image: "/images/categories/tshirt.jpeg", bg: "bg-green-900" },
+  { name: "Shirts", image: "/images/categories/shirt.jpeg", bg: "bg-gray-600" },
+  { name: "Hoodies", image: "/images/categories/hoodies.jpeg", bg: "bg-gray-300" },
+  { name: "Caps", image: "/images/categories/caps.jpeg", bg: "bg-white" },
 ];
 
 const Categories = () => {
